4fc3a5c4249075fa94a6a1ffd0197635b1292937
roma <marinahohulya3107@gmail.com>
2025-02-15 15:32:19 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,85 @@
+from pygame import *
+
+# клас-батько для спрайтів
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (65, 65))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+
+class Player(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_LEFT] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_RIGHT] and self.rect.x < win_width - 80:
+            self.rect.x += self.speed
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_width - 80:
+            self.rect.y += self.speed
+
+class Enemy(GameSprite):
+    direction = "left"
+
+    def update(self):
+        if self.rect.x <= 470:
+            self .direction = "right"
+        if self.rect.x >= win_width - 85:
+            self.direction = "left"
+
+        if self.direction == "left":
+            self.rect.x -= self.speed
+        else:
+            self.rect.x += self.speed
+# Ігрова сцена:
+win_width = 700
+win_height = 500
+
+window = display.set_mode((win_width, win_height))
+display.set_caption("Maze")
+background = transform.scale(image.load("background.jpg"),
+                             (win_width, win_height))
+
+# Персонажі гри:
+player = Player('hero.png', 5, win_height - 80, 4)
+monster = Enemy('cyborg.png', win_width - 80, 280, 2)
+final = GameSprite('treasure.png', win_width - 120, win_height - 80, 0)
+
+game = True
+clock = time.Clock()
+FPS = 60
+
+finish = False
+
+# музика
+mixer.init()
+mixer.music.load('jungles.ogg')
+mixer.music.play()
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+
+    if finish != True:
+
+        window.blit(background, (0, 0))
+
+        player.update()
+        monster.update()
+
+        player.reset()
+        monster.reset()
+
+    display.update()
+    clock.tick(FPS)
